bae0ae19f30d94c3e17c26c2aa4189039dd93ec6
anmc <lamylanmc@hotmail.com>
2020-07-08 15:02:39 +0800

    remove preload texture and update object

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1102,19 +1102,6 @@ function WebGLRenderer( parameters ) {
 	};
 	
 	// Compile async
-	function compileTextureAsync( material ){
-		
-		return new Promise( function( resolve, reject ){
-			requestAnimationFrame( function(){
-				material.map && textures.setTexture2D( material.map, 0 );
-				material.normalMap && textures.setTexture2D( material.normalMap, 0 );
-				material.roughnessMap && textures.setTexture2D( material.roughnessMap, 0 );
-				material.metalnessMap && textures.setTexture2D( material.metalnessMap, 0 );
-				resolve();
-			});
-		});
-	}
-	
 	this.compileAsync = function ( scene, camera, object, progress ) {
 		currentRenderState = renderStates.get( scene, camera );
 		currentRenderState.init();
@@ -1158,9 +1145,6 @@ function WebGLRenderer( parameters ) {
 							tasks.push( initMaterialAsync( material2, scene, object ).then( function(){
 								progress && progress( ++completedCnt/tasks.length );
 							}));
-							tasks.push( compileTextureAsync( material2 ).then( function(){
-								progress && progress( ++completedCnt/tasks.length );
-							}));
 							compiled[ material2.uuid ] = true;
 
 						}
@@ -1170,9 +1154,6 @@ function WebGLRenderer( parameters ) {
 				} else if ( material.uuid in compiled === false ) {
 
 					tasks.push( initMaterialAsync( material, scene, object ).then( function(){
-						progress && progress( ++completedCnt/tasks.length );
-					}));
-					tasks.push( compileTextureAsync( material ).then( function(){
 						progress && progress( ++completedCnt/tasks.length );
 					}));
 					compiled[ material.uuid ] = true;
@@ -1188,15 +1169,10 @@ function WebGLRenderer( parameters ) {
 					tasks.push( initMaterialAsync( customDepthMaterial, scene, object ).then( function(){
 						progress && progress( ++completedCnt/tasks.length );
 					}));
-					tasks.push( compileTextureAsync( customDepthMaterial ).then( function(){
-						progress && progress( ++completedCnt/tasks.length );
-					}));
 					compiled[ customDepthMaterial.uuid ] = true;
 
 				}
 			}
-
-			if( object.isMesh ) objects.update( object );
 		} );
 		
 		return Promise.all(tasks);
