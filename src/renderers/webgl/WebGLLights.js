01cd726e8e05c073de8c205a354c3668ac5f47ec
anmc <lamylanmc@hotmail.com>
2022-01-05 09:38:20 +0800

    fix undefined numSpotMaps

--- src/renderers/webgl/WebGLLights.js
+++ src/renderers/webgl/WebGLLights.js
@@ -686,7 +686,6 @@ function WebGLLights( extensions, capabilities, staticLightConfig ) {
 				const uniforms = state.spot[ spotLength ];
 
 				if(light.map){
-					uniforms.map = numSpotMaps;
 					var tanAngle = Math.tan(light.angle);
 					light.mapMatrix.makePerspective( -tanAngle, tanAngle, tanAngle, -tanAngle, 1, 10 );
 					light.mapMatrix.multiply( matrix4.copy(light.matrixWorld).invert() );
